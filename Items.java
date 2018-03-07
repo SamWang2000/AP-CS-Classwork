450502532462c855e4d338efd6fac6273d263ca2
SamWang2000 <34665649+SamWang2000@users.noreply.github.com>
2018-03-07 09:26:04 +0800

    Add files via upload

---
+++ Items.java
@@ -0,0 +1,52 @@
+package library;
+
+import java.util.Date;
+
+/**
+ *
+ * @author student-311
+ */
+public class Items {
+    private String title;
+    private String author_artist;
+    private int itemID;
+    private Boolean onloan;
+    private Date dueDate;
+    private Boolean requested;
+    
+    public Items(String Title, String Author_Artist, int Itemid, 
+            Boolean Onloan, Date Duedate){
+        Title = title;
+        Author_Artist = author_artist;
+        Itemid = itemID;
+        Onloan = onloan;
+        Duedate = dueDate;
+        
+    }
+    
+    public String GetTitle(){
+   return title;
+}
+    public String GetAuthor_Artist(){
+        return author_artist;
+    }
+    public int GetItemID(){
+        return itemID;
+    }
+    public Boolean Onloan(){
+        return onloan;
+    }
+    public Date DueDate(){
+        return dueDate;
+    }
+    public void printDetails(){
+        System.out.println("title"+title+"\n"+"author:"+author_artist+"reference number"+itemID);
+        if(onloan)
+            System.out.println("This book can be loaned");
+        else
+            System.out.println("This book cannot be loaned");   
+    }
+   
+    
+}
+    
